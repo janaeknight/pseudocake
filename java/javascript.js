aa70c12246fd1114583aaf5ce0171287c34084c1
Knight <janae.jpeg@gmail.com>
2019-10-07 14:33:15 -0500

    Initial commit

---
+++ java/javascript.js
@@ -0,0 +1,20 @@
+// Tree
+
+var x = document.getElementById("exotictraditional");
+var a = document.getElementById("slipperyslimy");
+var ab = document.getElementById("coolnot");
+var ac = document.getElementById("expensiveannoying");
+var q = document.getElementById("smallfree");
+var qw = document.getElementById("friendlyaggressive");
+var qe = document.getElementById("rightwrong");
+
+// var o = document.getElementById("");
+
+function exotic() {
+    x.style.display = "none";
+    a.style.display = "block";
+}
+
+document.getElementById("exotic").addEventListener("click", function(){
+    document.getElementById("demo").innerHTML = "Hello World";
+  });
